2f88832779e45e41663d2718d733e6a8b5924a79
Eliane <ecastelarstreater@gmail.com>
2026-08-19 15:40:34 +0200

    Update portfolio projects and gifs

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+// HERO LETTERS ANIMATION
+
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let isAnimating = false;
 
 document.querySelector(".text_about").onmouseover = (event) => {
+  if (isAnimating) return;
+
+  isAnimating = true;
+
   console.log(event.target.dataset.value.length);
   let iterations = 0;
   const interval = setInterval(() => {
@@ -14,15 +21,46 @@ document.querySelector(".text_about").onmouseover = (event) => {
       })
       .join("");
 
-    if (iterations >= event.target.dataset.value.length)
+    if (iterations >= event.target.dataset.value.length) {
       clearInterval(interval);
+      isAnimating = false;
+    }
 
     iterations += 1 / 2;
   }, 30);
 };
 
+// CUSTOM CURSOR
+
 const cursor = document.querySelector(".cursor");
 document.addEventListener("mousemove", (e) => {
   cursor.style.left = e.pageX + "px";
   cursor.style.top = e.pageY + "px";
+});
+
+// CAROUSEL
+
+const btnLeft = document.querySelector(".carousel_btn_left");
+const btnRight = document.querySelector(".carousel_btn_right");
+
+const carouselItemsArray = [...document.querySelectorAll(".carousel_item")];
+
+let currentIndex = 0;
+
+function updateCarousel() {
+  carouselItemsArray.forEach((item, index) => {
+    item.classList.toggle("active", index == currentIndex);
+    item.classList.toggle("inactive", index !== currentIndex);
+  });
+}
+
+btnRight.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % carouselItemsArray.length;
+  updateCarousel();
+});
+
+btnLeft.addEventListener("click", () => {
+  currentIndex =
+    (currentIndex - 1 + carouselItemsArray.length) % carouselItemsArray.length;
+  updateCarousel();
 });
